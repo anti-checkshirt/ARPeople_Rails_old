77d1e9a69914c9be48864f49a98a374b0336327c
s0ralit <s0ralit@protonmail.com>
2018-12-16 04:47:22 +0900

    動かないので直しました

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -22,6 +22,7 @@ class Api::V1::UserController < ApplicationController
   def show
     image_key_names = [params[:image1], params[:image2], params[:image3], params[:image4], params[:image5], params[:image6], params[:image7], params[:image8], params[:image9], params[:image10]]
     image_key_names.each do |image_key_name|
+      image = image_key_name
       File.binwrite("public/#{params[:id]}/#{SecureRandom.uuid}", image.read)
     end
   end
