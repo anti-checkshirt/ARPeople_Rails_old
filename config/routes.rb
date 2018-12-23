9b2739760282a6bfd9b2a5af362c9c8cbf699da4
s0ralit <s0ralit@protonmail.com>
2018-12-24 02:16:03 +0900

    コードミス

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, {format: 'json'} do
     namespace :v1 do
-      resources :user, only: :create :put
+      resources :user, only: :create
       post "setting" => "setting#show"
       post "search" => "search#show"
       get "image" => "image#show"
