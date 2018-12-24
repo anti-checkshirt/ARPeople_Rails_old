9a6cdff460f91180c913f8d6d8f1bbca6b91c8a8
s0ralit <s0ralit@protonmail.com>
2018-12-25 01:32:47 +0900

    uuidカラムをUserに追加

--- db/migrate/20181215050323_create_users.rb
+++ db/migrate/20181215050323_create_users.rb
@@ -4,10 +4,11 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :email
       t.string :password_digest
+      t.string :access_token
+      t.string :uuid
       t.string :twitter_id, default: ''
       t.string :github_id, default: ''
       t.string :person_id, default: ''
-      t.string :access_token
 
       t.timestamps
     end
