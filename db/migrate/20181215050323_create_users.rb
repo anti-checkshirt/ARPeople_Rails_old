ab6eeb1a523a484fe19f005d976edc64c27b04da
s0ralit <s0ralit@protonmail.com>
2018-12-25 02:50:48 +0900

    変更

--- db/migrate/20181215050323_create_users.rb
+++ db/migrate/20181215050323_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :email
       t.string :password_digest
       t.string :access_token
-      t.string :uuid, default: ''
+      t.string :uuid
       t.string :twitter_id, default: ''
       t.string :github_id, default: ''
       t.string :person_id, default: ''
