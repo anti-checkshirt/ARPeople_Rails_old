ddc712466efcb68561a79a8f00a5bddec90d5d51
s0ralit <s0ralit@protonmail.com>
2018-12-24 16:34:49 +0900

    条件分岐を追加し、不正なリクエストの場合jsonを返すようにした

--- app/controllers/api/v1/image_controller.rb
+++ app/controllers/api/v1/image_controller.rb
@@ -1,9 +1,13 @@
 
 class Api::V1::ImageController < ApplicationController
   def show
-    image_name = params[:image_name]
-    user_id = params[:user_id]
-    path = "/#{user_id}/#{image_name}"
-    render :html => "<img src='#{path}'>".html_safe
+    @image_name = params[:image_name]
+    @user_id = params[:user_id]
+    if @image_name.nil? or @user_id.nil?
+      response_bad_request
+    else
+      @path = "/#{@user_id}/#{@image_name}"
+      render :html => "<img src='#{@path}'>".html_safe
+    end
   end
 end
