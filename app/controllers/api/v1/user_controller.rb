2b06395d1d70d740bcb5b970d18a883e683c3edb
s0ralit <s0ralit@protonmail.com>
2019-02-02 00:56:10 +0900

    passwordをhash化

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UserController < ApplicationController
     @user = User.new(
       name: params[:name],
       email: params[:email],
-      password_digest: params[:password],
+      password: params[:password],
       age: "",
       user_image_url: "",
       twitter_id: "",
